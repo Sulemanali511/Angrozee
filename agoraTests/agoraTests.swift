e2debbdc8ba7ad1b6bb990e61c70efcbf15fc05e
@sulemanalibuiltin <suleman.ali@builtinsoft.com>
2021-05-29 11:28:39 +0500

    Initial Commit

---
+++ agoraTests/agoraTests.swift
@@ -0,0 +1,33 @@
+//
+//  agoraTests.swift
+//  agoraTests
+//
+//  Created by Suleman Ali on 29/05/2021.
+//
+
+import XCTest
+@testable import agora
+
+class agoraTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
